bcb57dfc18aedea36a6571af21fad00ea82d6656
marouane mesned <raihanemesned@gmail.com>
2021-06-12 13:12:54 +0200

    n'importe quoi

--- imports/ui/Accueil/accueil.js
+++ imports/ui/Accueil/accueil.js
@@ -3,3 +3,20 @@ import { Template } from 'meteor/templating';
 import "./accueil.html";
 import "./accueil.css"; 
 import "./template.js";
+
+Session.set('menu', '');
+
+Template.accueil.events({
+    'click .afficher-le-menu'(event){
+
+        if (Session.get('menu')==='menu') {Session.set('menu','')}
+        else {Session.set('menu', 'menu')} 
+
+}});   
+
+Template.accueil.helpers({
+
+//template dynamique
+menuonoff: function(menu){
+    return Session.get('menu');
+}});
